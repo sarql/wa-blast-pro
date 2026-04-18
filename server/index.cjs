35a114b5274d401ebedb80cb09974b39d57991dc
sarql <team.sarql@gmail.com>
2026-04-18 21:38:15 +0530

    fix chromium path

--- server/index.cjs
+++ server/index.cjs
@@ -26,10 +26,15 @@ const client = new Client({
     authStrategy: new LocalAuth({ dataPath: './sessions' }),
     puppeteer: {
         headless: true,
+        executablePath: '/usr/bin/chromium-browser',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
             '--disable-extensions'
         ]
     }
